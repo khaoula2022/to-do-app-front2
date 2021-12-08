6b45b2722d300df9f512b92f9527e433e0ee20e0
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 22:26:40 +0000

    adding delete reducer

--- src/api/tasksApi.js
+++ src/api/tasksApi.js
@@ -5,4 +5,4 @@ export const fetchTasks = () => Axios().get(url);
 export const CreateTask = (newTask) => Axios().post(url, newTask);
 export const UpdateTaskApi = (id, updatedTask) =>
   Axios().patch(`${url}/${id}`, updatedTask);
-export const deleteTask = (id) => Axios().delete(`${url}/${id}`);
+export const deleteTaskApi = (id) => Axios().delete(`${url}/${id}`);
