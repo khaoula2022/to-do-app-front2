08c3cd5b930f507571fb92ed55c976d85d9c1d60
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 17:21:34 +0000

    Create an empty Card to display tasks

---
+++ src/appBase/tasks/singleTask.js
@@ -0,0 +1,23 @@
+import {
+  CardHeader,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from "@material-ui/core";
+import React from "react";
+
+function singleTask() {
+  return (
+    <div>
+      <Card>
+        <CardHeader></CardHeader>
+        <CardMedia></CardMedia>
+        <CardContent></CardContent>
+        <CardActions></CardActions>
+      </Card>
+    </div>
+  );
+}
+
+export default singleTask;
