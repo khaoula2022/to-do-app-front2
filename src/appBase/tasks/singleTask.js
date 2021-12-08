7d8d84015ff7b62d9c0bf2f3167bf2d7ab4c09b7
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 17:37:24 +0000

    adding a grid to display the tasks

--- src/appBase/tasks/singleTask.js
+++ src/appBase/tasks/singleTask.js
@@ -11,7 +11,7 @@ import { useHistory } from "react-router";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
-function SingleTask({ tasks }) {
+function SingleTask({ tasks, setCurrentId }) {
   const dispatch = useDispatch();
   const history = useHistory();
 
--- src/appBase/tasks/tasksList.js
+++ src/appBase/tasks/tasksList.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectTasks } from "./taskSlice";
+import SingleTask from "./SingleTask";
+import { Grid } from "@material-ui/core";
 
-function tasksList() {
-  return <div></div>;
+function TasksList({ setcurrentId }) {
+  const tasks = useSelector(selectTasks);
+
+  return (
+    <Grid>
+      {tasks.map((task) => (
+        <Grid key={task?._id}>
+          <SingleTask tasks={task} setCurrentId={setcurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
 }
 
-export default tasksList;
+export default TasksList;
