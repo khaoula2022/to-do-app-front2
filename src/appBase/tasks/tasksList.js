22264f3f1d9e990bc6ea3426b59e72ebe9d8b397
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 18:04:45 +0000

    changing the layout

--- src/appBase/tasks/tasksList.js
+++ src/appBase/tasks/tasksList.js
@@ -3,14 +3,20 @@ import { useSelector } from "react-redux";
 import { selectTasks } from "./taskSlice";
 import SingleTask from "./SingleTask";
 import { Grid } from "@material-ui/core";
+import useStyles from "./tasksListStyles";
 
 function TasksList({ setcurrentId }) {
   const tasks = useSelector(selectTasks);
-
+  const classes = useStyles;
   return (
-    <Grid>
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
       {tasks.map((task) => (
-        <Grid key={task?._id}>
+        <Grid key={task?._id} item xs={12} sm={6} md={2}>
           <SingleTask tasks={task} setCurrentId={setcurrentId} />
         </Grid>
       ))}
