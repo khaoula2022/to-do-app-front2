131dc4166f96b7b7d34e4f993155b4442747041d
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 23:59:29 +0000

    adding register Function

--- src/appBase/user/services/auth.service.js
+++ src/appBase/user/services/auth.service.js
@@ -1,3 +1,26 @@
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 const API_URL = "http://localhost:4000/user/signupg";
+const register = (
+  username,
+  email,
+  password,
+  confirmpassword,
+
+  avatar
+) => {
+  return axios
+    .post("http://localhost:4000/user/signupg", {
+      username,
+      email,
+      password,
+      confirmpassword,
+      avatar,
+    })
+    .then((response) => {
+      console.log("local storage");
+      localStorage.setItem("user", JSON.stringify(response.data));
+
+      return response.data;
+    });
+};
