4a0a81a95a119bc3539ca4599ce2b1e4b399fadc
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 22:43:08 +0000

    Update ConfirmDialog.js

--- src/appBase/confirmation/ConfirmDialog.js
+++ src/appBase/confirmation/ConfirmDialog.js
@@ -1,7 +1,51 @@
 import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
 
-function ConfirmDialog() {
-  return <div></div>;
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    padding: theme.spacing(2),
+    position: "absolute",
+    top: theme.spacing(30),
+  },
+  dialogTitle: {
+    textAlign: "center",
+  },
+  dialogContent: {
+    textAlign: "center",
+  },
+  dialogAction: {
+    justifyContent: "center",
+  },
+}));
+
+export default function ConfirmDialog(props) {
+  const { confirmDialog, setConfirmDialog } = props;
+  const classes = useStyles();
+
+  return (
+    <Dialog open={confirmDialog.isOpen} classes={{ paper: classes.dialog }}>
+      <DialogTitle className={classes.dialogTitle}></DialogTitle>
+      <DialogContent className={classes.dialogContent}>
+        <Typography variant="h6">{confirmDialog.title}</Typography>
+        <Typography variant="subtitle2">{confirmDialog.subTitle}</Typography>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => setConfirmDialog({ ...confirmDialog, isOpen: false })}
+        >
+          {" "}
+          Close
+        </Button>
+        <button onClick={confirmDialog.onConfirm}> Confirm </button>
+      </DialogActions>
+    </Dialog>
+  );
 }
-
-export default ConfirmDialog;
