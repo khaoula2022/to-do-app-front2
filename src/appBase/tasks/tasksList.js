64f323ad9ef34be109a9d3c1d242edf986ec6ed6
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 16:57:16 +0000

    creating taskList component

---
+++ src/appBase/tasks/tasksList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectcourses } from "./taskSlice";
+
+function tasksList() {
+  return <div></div>;
+}
+
+export default tasksList;
