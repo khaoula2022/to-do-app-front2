865df4b5f68aef71723f9aa239a55f9dbabc5bdd
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 13:12:23 +0000

    adding reduxjs/toolkit dependency

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
+import store from './utils/store';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
