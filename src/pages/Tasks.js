ca79a80dd7d520d7a3522f97de5350fb5a8c47f4
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 17:44:14 +0000

    displaying multiple tasks inside a list of tasks compoment

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,7 +1,26 @@
 import styles from "./Tasks.module.css";
+import TasksList from "./../appBase/tasks/TasksList";
+import { GetTasks } from "../appBase/tasks/taskSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
+import { useState } from "react";
+import { selectTasks } from "./../appBase/tasks/taskSlice";
 function Tasks() {
-  return <div className={styles.fluid}></div>;
+  const [currentId, setcurrentId] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(GetTasks());
+  }, [dispatch]);
+  return (
+    <div className={styles.fluid}>
+      <div className={styles.courses}>
+        {" "}
+        <TasksList setcurrentId={setcurrentId} />{" "}
+      </div>
+    </div>
+  );
 }
 
 export default Tasks;
