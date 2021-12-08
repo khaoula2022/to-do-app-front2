b4ec9815b71f592bbbf36a4ce25c000109e987b2
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 15:27:04 +0000

    fixing the routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { Suspense } from "react";
 import AppBase from "./appBase/AppBase";
+import Loader from "./pages/Loader";
+import Tasks from './pages/Tasks';
 
 import {
   Route,
@@ -10,21 +12,29 @@ import {
   useHistory,
   useLocation,
   NavLink,
-  Router,
+  Router
 } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Suspense fallback={<Loader></Loader>}>
       <Router>
         <Switch>
-          <Route path="/app">
-            <AppBase />
+          <Route exact path="/">
+            <Tasks />
           </Route>
+
         </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Tasks />
+          </Route>
+       
+        </Switch>
+        )
       </Router>
-    </div>
-  );
+    </Suspense>
+  
 }
 
 export default App;
