6d2e0ca594bccc2c3f23dd17f67af0b356f7f6be
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 23:24:45 +0000

    adding some boilerplate code for the design

--- src/appBase/appBase.js
+++ src/appBase/appBase.js
@@ -6,7 +6,7 @@ import {
   useLocation,
   NavLink,
 } from "react-router-dom";
-import { blue } from "@material-ui/core/colors";
+import { red } from "@material-ui/core/colors";
 import {
   Avatar,
   ClickAwayListener,
@@ -27,6 +27,7 @@ import AppBar from "@material-ui/core/AppBar";
 
 import React, { useEffect, useState } from "react";
 import Tasks from "./../pages/Tasks";
+import HomePage from "./../pages/HomePage";
 const drawerWidth = 200;
 
 const useStyles = makeStyles((theme) => ({
@@ -34,7 +35,7 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     gap: "15px",
   },
-  link: { textDecoration: "none", color: "#36454F" },
+  link: { textDecoration: "none", color: "#875" },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     backgroundColor: "#fff",
@@ -43,20 +44,7 @@ const useStyles = makeStyles((theme) => ({
     width: "100%",
     justifyContent: "space-between",
   },
-  logo: {
-    height: "40px",
-    cursor: "pointer",
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
-  },
+
   content: {
     width: "100%",
     padding: theme.spacing(2),
@@ -68,7 +56,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: "20px",
   },
   avatar: {
-    backgroundColor: blue[700],
+    backgroundColor: red[700],
     cursor: "pointer",
   },
 }));
@@ -83,11 +71,14 @@ export default function AppBase() {
           <NavLink to={`/app/tasks`}> </NavLink>
           <Switch>
             <Route exact path="/app">
-              <Redirect to="/app/tasks"></Redirect>
+              <Redirect to="/app/home"></Redirect>
             </Route>
 
             <Route exact path="/app/tasks">
               <Tasks></Tasks>
+            </Route>
+            <Route exact path="/app/home">
+              <HomePage></HomePage>
             </Route>
           </Switch>
         </Toolbar>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import styles from "./home.module.css";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
 function HomePage() {
-    return (
+  const history = useHistory();
+
+  const useStyles = makeStyles((theme) => ({
+    "@global": {
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: "none",
+      },
+    },
+
+    toolbar: {
+      flexWrap: "wrap",
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+  }));
+  const classes = useStyles();
+  return (
+    <div className={styles.LandingPage}>
+      <div className={styles.nav}>
         <div>
-            
+          <>
+            <button
+              className={styles.signupButton}
+              onClick={() => history.push("/signup")}
+            >
+              Sign Up
+            </button>
+            or
+            <button
+              className={styles.signinButton}
+              onClick={() => history.push("/login")}
+            >
+              Sign in
+            </button>
+          </>
+          <></>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
