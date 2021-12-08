5cb6790bfa9e90151d9ab794097b1f065630e6f9
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 14:56:12 +0000

    adding the routes to the appBase file

--- src/appBase/appBase.js
+++ src/appBase/appBase.js
@@ -7,8 +7,21 @@ import {
   NavLink,
 } from "react-router-dom";
 import React, { useEffect, useState } from "react";
-import Tasks from "../Pages/Tasks";
+import Tasks from "./../pages/Tasks";
 
 export default function AppBase() {
-  return <></>;
+  return (
+    <>
+      <NavLink to={`/app/tasks/`}>Consulter les taches ici</NavLink>
+
+      <Switch>
+        <Route exact path="/app">
+          <Redirect to="/app/tasks"></Redirect>
+        </Route>
+        <Route exact path="/app/courses">
+          <Tasks></Tasks>
+        </Route>
+      </Switch>
+    </>
+  );
 }
