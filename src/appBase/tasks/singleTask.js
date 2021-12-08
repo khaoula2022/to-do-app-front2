bbad8ece8b749bd29e765baafc89d45845fc523f
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 21:53:02 +0000

    customizing icons using material UI icons

--- src/appBase/tasks/singleTask.js
+++ src/appBase/tasks/singleTask.js
@@ -5,7 +5,10 @@ import {
   CardContent,
   CardActions,
   Typography,
+  IconButton,
 } from "@material-ui/core";
+import EditIcon from "@material-ui/icons/Edit";
+
 import React from "react";
 import { useHistory } from "react-router";
 import { useState } from "react";
@@ -24,8 +27,16 @@ function SingleTask({ tasks, setCurrentId }) {
   return (
     <div>
       <Card className={styles.card}>
-        <CardHeader title={tasks.label}></CardHeader>
-        <CardMedia className={styles.media}></CardMedia>
+        <CardHeader
+          title={tasks.label}
+          subheader={tasks.createdAt}
+          action={
+            <IconButton aria-label="settings">
+              <EditIcon onClick={() => setCurrentId(tasks._id)} />
+            </IconButton>
+          }
+        ></CardHeader>
+        <CardMedia className={styles.media} title={tasks.label}></CardMedia>
         <CardContent>
           <Typography variant="body2" color="textSecondary" component="p">
             {tasks.description}
