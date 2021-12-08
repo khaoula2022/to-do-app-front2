5a82deb544a8037e837061a27949f09657c60311
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-08 16:24:51 +0000

    added Navlink to appBase.js

--- src/appBase/appBase.js
+++ src/appBase/appBase.js
@@ -3,12 +3,16 @@ import {
   Switch,
   Redirect,
   useHistory,
-  NavLink,
   useLocation,
+  NavLink,
 } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Tasks from "./../pages/Tasks";
 
 export default function AppBase() {
-  return <></>;
+  return (
+    <>
+      <NavLink to={`/app/tasks`}> Consulter mes taches</NavLink>
+    </>
+  );
 }
