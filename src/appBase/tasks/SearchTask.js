da7065458a9dfb14bd29e3a822343a60e23854e6
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-09 21:22:41 +0000

    Update SearchTask.js

--- src/appBase/tasks/SearchTask.js
+++ src/appBase/tasks/SearchTask.js
@@ -1,1 +1,8 @@
 import React, { useState } from "react";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+function SearchPage({ setTitle, title }) {
+    return (
+     
+    );
+  }
