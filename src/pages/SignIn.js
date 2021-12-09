92557edd5cceb61a1c52f0e2c7708865a91f8456
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-09 16:00:54 +0000

    Update SignIn.js

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,6 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import styles from "./SignUp.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function SignIn() {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const [values, setValues] = React.useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return <div></div>;
 }
 
