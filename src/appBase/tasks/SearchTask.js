8fc9ab23900607a790388a3e3243807567a84d5d
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-09 21:24:01 +0000

    added some design to the search Bar

--- src/appBase/tasks/SearchTask.js
+++ src/appBase/tasks/SearchTask.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Autocomplete from "@material-ui/lab/Autocomplete";
+import FindInPageIcon from "@material-ui/icons/FindInPage";
+import styles from "./SearchBar.module.css";
+
 function SearchPage({ setTitle, title }) {
-    return (
-     
-    );
-  }
+  return (
+    <div className={styles.header__search}>
+      <FindInPageIcon></FindInPageIcon>
+      <input
+        placeholder="Type your keyword here "
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="px-16"
+        disableUnderline
+        fullWidth
+      />
+    </div>
+  );
+}
