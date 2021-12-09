8aa384906867cbd13227c1690c4b4c539e6dcbf4
khaoula2022 <khaoula.khmiri@esprit.tn>
2021-12-09 01:12:12 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,32 +15,29 @@ import HomePage from "./pages/HomePage";
 import SignUp from "./pages/SignUp";
 
 function App() {
-  const user = useSelector((state) => state.user.user);
+  // const user = useSelector((state) => state.user.user);
 
   const AppBase = React.lazy(() => import("../src/appBase/AppBase"));
 
   return (
     <Suspense fallback={<Loader></Loader>}>
       <Router>
-        {!user ? (
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route exact path="/signup">
-              <SignUp />
-            </Route>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route exact path="/">
-              <LandingPage />
-            </Route>
-            <Route path="/app">
-              <AppBase />
-            </Route>
-          </Switch>
-        )}
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+        </Switch>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/app">
+            <AppBase />
+          </Route>
+        </Switch>
       </Router>
     </Suspense>
   );
